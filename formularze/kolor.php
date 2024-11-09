60eb18da9102138877675be40d51493177329826
Pandorciaa <matuszak.martyna@o2.pl>
2024-11-09 17:37:06 +0100

    END

---
+++ formularze/kolor.php
@@ -0,0 +1,40 @@
+<?php
+include_once __DIR__ . "/../laczenie_z_baza.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>Tu dodajesz kolor swojej rybuni!</h1>
+    <nav>
+        <a href="./formularze/ryby.php">Dodaj moją rybę</a>
+        <a href="./../index.php">Strona główna</a>
+        <a href="./formularze/gatunek.php">gatunki</a>
+    </nav>
+    </header>
+    <main>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <label for="kolor">wpisz kolor</label>
+        <input type="text" name="kolor">
+        <button type="submit">zatwierdź</button>
+        </form>
+        <?php 
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $kolor=$_POST['kolor'];
+        $kwerenda="insert into kolory(kolor) values ('$kolor')";
+        if($conn->query($kwerenda)){
+            echo 'dodano';
+        } else {
+           echo 'nie dodano :(';
+        }
+        };
+        ?>
+    </main>
+</body>
+</html>
