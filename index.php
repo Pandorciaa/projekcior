266fdbdd79d6479814d308ee0f3f414eb4d4ba7b
Pandorciaa <matuszak.martyna@o2.pl>
2024-11-09 16:59:09 +0100

    dodano gore

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rybkowo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <h1>Witaj w rybkowie!</h1>
+    <nav>
+        <a href="./formularze/ryby.php">Dodaj moją rybę</a>
+        <a href="./formularze/kolor.php">kolor</a>
+        <a href="./formularze/gatunek.php">gatunki</a>
+    </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
